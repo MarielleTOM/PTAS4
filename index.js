99ab0a13626595d87c451bd4492a5ec9af35c99a
MarielleTOM <marielle.martins@estudante.ifms.edu.br>
2024-12-12 13:10:06 -0400

    11/12

--- index.js
+++ index.js
@@ -10,6 +10,17 @@ app.use(cors());
 //Responde a qualuqer requisição encaminhada para 
 // /auth/algumaCoisa 
 const authRoutes = require("./routes/authRoutes");
+const AuthController = require("./controllers/AuthController");
 app.use("/auth", authRoutes);
+
+app.get("/meus-pedidos", (req, res) => {
+    res.send("Veja seus pedidos abaixo:");
+});
+
+app.get("/pedidos/55845125", (req, res) => {});
+
+app.get("/meu-perfil", (req, res) => {});
+
+
 
 app.listen(8000);
