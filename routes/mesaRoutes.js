ed7ecb34c031d3b4e287468f578ad8d4f9e15481
Anielly Teixeira <lisagoncalves.1977@gmail.com>
2025-01-23 21:01:16 -0300

    Verificar usuário Adm

---
+++ routes/mesaRoutes.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const router = require("express").Router ();
+
+const MesaController = require("../controllers/MesaController");
+const AuthController = require("../controllers/AuthController");
+
+router.post(
+"/novo",
+ AuthController.autenticar,
+ AuthController.verificaPermissaoAdm,
+ AuthController.novaMesa
+);
+
+module.exports = router;
