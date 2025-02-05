dcdc19eb321f9e95eaf6df34614cdece3b0d4058
MarielleTOM <marielle.martins@estudante.ifms.edu.br>
2025-02-05 11:58:54 -0400

    Commit
    mudanças nas rotas de mesa

--- routes/mesaRoutes.js
+++ routes/mesaRoutes.js
@@ -1,20 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const MesaController = require("../controllers/MesaController");
-const AuthController = require("../controllers/AuthController");
-
-// Cadastrar Nova Mesa (restrito ao adm)
-router.post(
-  "/novo",
-  AuthController.verificaAutenticacao,
-  AuthController.verificaPermissaoAdm,
-  MesaController.novaMesa
-);
-
-// Buscar Mesas (sem necessidade de autenticação)
+const AuthController = require("../controllers/AuthController")
+router.post("/novo", 
+    AuthController.verificaAutenticacao, 
+    AuthController.verificaPermissaoAdm, 
+    MesaController.novaMesa);
 router.get("/", MesaController.buscarMesas);
-
-// Consultar Mesas Disponíveis por Data (sem necessidade de autenticação)
-router.get("/disponibilidade", MesaController.consultarDisponibilidade);
-
+router.get("/disponibilidade", MesaController.mesasDisp);
 module.exports = router;
