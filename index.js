1eb241011a8566f09a802e33907d317b1157abc3
MarielleTOM <marielle.martins@estudante.ifms.edu.br>
2025-02-05 12:00:18 -0400

    commit
    mudanças no index

--- index.js
+++ index.js
@@ -1,33 +1,24 @@
 const prisma = require("./prisma/prismaClient");
-const express = require('express');
-
+const express = require("express");
 const cors = require("cors");
+
 const AuthController = require("./controllers/AuthController")
 
 const app = express();
 app.use(express.json());
-
 app.use(cors());
 
 const authRoutes = require("./routes/authRoutes");
 app.use("/auth", authRoutes);
 
-const perfilRoutes = require("./routes/perfilRoutes");
-app.use("/perfil", AuthController.verificaAutenticacao, perfilRoutes);
+const profileRoutes = require("./routes/PerfilRoutes"); 
+app.use("/perfil", AuthController.verificaAutenticacao, profileRoutes);
 
-const mesaRoutes = require("./routes/mesaRoutes");
-app.use("/mesa", mesaRoutes);
+const mesaRoutes = require("./routes/mesaRoutes"); 
+app.use("/mesa", mesaRoutes); 
 
-const reservaRoutes = require("./routes/reservaRoutes");
-app.use("/reservas", AuthController.verificaAutenticacao, reservaRoutes);
-
-app.get("/meus-pedidos", (req, res) => {
-    res.send("Veja seus pedidos abaixo:");
-});
-
-app.get("/pedidos/55845125", (req, res) => { });
-
-app.get("/meu-perfil", (req, res) => { });
+const reservaRoutes = require("./routes/reservaRoutes"); 
+app.use("/reserva", AuthController.verificaAutenticacao, reservaRoutes)
 
 app.listen(8000, () => {
     console.log("Servidor rodando na porta 8000");
