e887caa485fe03b647186e18fc2d318c3af8f3ff
MarielleTOM <marielle.martins@estudante.ifms.edu.br>
2025-02-05 11:58:26 -0400

    Commit
    mudanças nas rotas de authController

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,6 @@
-const express = require("express")
-const router = require("express").Router ();
-
+const express = require("express");
+const router = express.Router();
 const AuthController = require("../controllers/AuthController");
-
+router.post("/login", AuthController.login);
 router.post("/cadastro", AuthController.cadastro);
-router.post('/login', AuthController.login);
-
 module.exports = router;
